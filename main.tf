2302080631fd0d95015ec301e89ca3fadb70e6d9
Vinicius Kirst <vinicius.kirst@gmail.com>
2022-01-22 22:03:24 -0300

    organizing variables

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "oci" {
-  tenancy_ocid = var.oracle_tenancy_ocid
-  user_ocid = var.oracle_user_ocid
-  fingerprint = var.oracle_fingerprint
-  private_key = var.oracle_private_key
-  region = var.oracle_region
+#   tenancy_ocid = var.oracle_tenancy_ocid
+#   user_ocid = var.oracle_user_ocid
+#   fingerprint = var.oracle_fingerprint
+#   private_key = var.oracle_private_key
+#   region = var.oracle_region
 }
 
 resource "oci_core_vcn" "vcn" {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
 variable "prefix" {
     default = "mynetwork"
+    description = "Prefix for the network resources"
+}
+
+variable "oracle_compartment_id" {
+    description = "The OCID of the compartment to create the network in"
+}
+
+variable "oracle_account_email" {
+    description = "The email of the Oracle Cloud account"
+}
+
+variable "my_ip" {
+    description = "The IP address allowed for ssh"
+    default = "0.0.0.0/0"
 }
