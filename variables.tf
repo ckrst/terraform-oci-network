9876585b12a70fbce219a5d99f34b5c001e3f84d
Vinicius Kirst <vinicius.kirst@gmail.com>
2022-01-22 22:12:32 -0300

    provider configuration

--- variables.tf
+++ variables.tf
@@ -11,6 +11,26 @@ variable "oracle_account_email" {
     description = "The email of the Oracle Cloud account"
 }
 
+variable "oracle_tenancy_ocid" {
+    description = "The OCID of the tenancy to create the network in"
+}
+
+variable "oracle_user_ocid" {
+    description = "The OCID of the user to create the network in"
+}
+
+variable "oracle_fingerprint" {
+    description = "The fingerprint of the user to create the network in"
+}
+variable "oracle_private_key" {
+    description = "The private key of the user to create the network in"
+}
+variable "oracle_region" {
+    description = "The region to create the network in"
+}
+
+
+
 variable "my_ip" {
     description = "The IP address allowed for ssh"
     default = "0.0.0.0/0"
